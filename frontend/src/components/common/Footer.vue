ea47bcdecaadf4c3bd6d26596befc75a15b2df99
한훈희 <hanjoi77@naver.com>
2021-09-17 20:50:34 +0900

    [S05P21B204-125] feat: Update layout forms

---
+++ frontend/src/components/common/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-footer reveal bordered class="bg-white text-grey-8">
+    <q-toolbar>
+      <q-toolbar-title class="row justify-around font-center items-center">
+        <div class="col-gutter-none">
+          <div class="text-center">
+            <q-avatar text-color="grey" size="xl" icon="home" />
+          </div>
+          <p class="no-margin text-center" style="font-size: 12px">홈</p>
+        </div>
+        <div class="col-gutter-none">
+          <div class="text-center">
+            <q-avatar text-color="grey" size="xl" icon="explore" />
+          </div>
+          <p class="no-margin text-center" style="font-size: 12px">내주변</p>
+        </div>
+        <div class="col-gutter-none content-center">
+          <q-avatar color="primary" size="xl" text-color="white" icon="search" />
+        </div>
+        <div class="col-gutter-none">
+          <div class="text-center">
+            <q-avatar text-color="grey" size="xl" icon="favorite" />
+          </div>
+          <p class="no-margin text-center" style="font-size: 12px">찜</p>
+        </div>
+        <div class="col-gutter-none">
+          <div class="text-center">
+            <q-avatar text-color="grey" size="xl" icon="person" />
+          </div>
+          <p class="no-margin text-center" style="font-size: 12px">MY</p>
+        </div>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+
+</style>
