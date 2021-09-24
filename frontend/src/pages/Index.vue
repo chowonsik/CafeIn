3199e12de414501dbca0d4ccc79ce7089356826f
한상정 <hsj7630@daum.net>
2021-09-24 17:37:01 +0900

    [S05P21B204-140] add: mainpage

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,50 +1,75 @@
 <template>
-<div class="q-pa-md">
-    <q-carousel
-      arrows
-      animated
-      v-model="slide"
-      height="25vh"
-    >
-      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">First stop</div>
-          <div class="text-subtitle1">Mountains</div>
+  <div>
+    <main-carousel />
+    <div>
+      <div class="row justify-around font-center items-center" style="height: 6rem">
+        <div>
+          <q-avatar>
+            <img src="../assets/image/coffeecup.png">
+          </q-avatar>
+          <p class="no-margin text-center">커피</p>
         </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">Second stop</div>
-          <div class="text-subtitle1">Famous City</div>
+        <div>
+          <q-avatar>
+            <img src="../assets/image/openbook.png">
+          </q-avatar>
+          <p class="no-margin text-center">공부</p>
         </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">Third stop</div>
-          <div class="text-subtitle1">Famous Bridge</div>
+        <div>
+          <q-avatar>
+            <img src="../assets/image/love.png">
+          </q-avatar>
+          <p class="no-margin text-center">데이트</p>
         </div>
-      </q-carousel-slide>
-    </q-carousel>
+        <div>
+          <q-avatar>
+            <img src="../assets/image/camera.png">
+          </q-avatar>
+          <p class="no-margin text-center">사진</p>
+        </div>
+      </div>
+    </div>
+    <div style="margin-bottom: 0.5rem">
+      <q-img
+          src="../assets/image/cafeimage5.jpg"
+          :ratio="16/9"
+          style="max-height: 220px;"
+      >
+      <div class="absolute-full custom-caption">
+        <div class="text-h5">카페 큐레이션</div>
+        <div class="text-subtitle1">태그를 통해 카페 추천을 받으세요</div>
+      </div>
+      </q-img>
+    </div>
+    <div>
+      <q-img
+        src="../assets/image/coffeeimage1.jpg"
+        :ratio="16/9"
+        style="max-height: 220px;"
+      >
+      <div class="absolute-full custom-caption">
+        <div class="text-h4">나에게 맞는</div>
+        <div class="text-h4">카페 추천</div>
+      </div>
+      </q-img>
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { ref } from 'vue'
+import MainCarousel from '../components/mainpage/MainCarousel.vue'
 
 export default defineComponent({
   name: 'PageIndex',
-  setup () {
-  return {
-    slide: ref('first')
+  components: {
+    MainCarousel
   }
-}
 })
 </script>
 
 <style lang="sass" scoped>
 .custom-caption
-  text-align: center
   padding: 12px
   color: white
   background-color: rgba(0, 0, 0, .3)
