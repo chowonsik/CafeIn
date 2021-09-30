00195f68a1d591c4a4bb5df20676c20eed49b18a
한훈희 <hanjoi77@naver.com>
2021-09-30 17:30:41 +0900

    [S05P21B204-190] feat: Add Kakao map init

---
+++ frontend/src/components/common/KakaoMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div id="map" style="width:1000px;height:500px;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KakaoMap",
+  data() {
+    return {
+      map: null,
+      markers: [],
+    }
+  },
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src =
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=1582a2def7ba0fb35b9d01fe07ea8c10";
+      document.head.appendChild(script);
+    }
+  },
+  methods: {
+    initMap() {
+      // 지도를 표시할 div
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 5, // 지도의 확대 레벨
+      };
+      this.map = new kakao.maps.Map(container, options);
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,7 +2,10 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    children: [
+      { path: "", component: () => import("pages/Index.vue") },
+      { path: "mycafe", component: () => import("pages/MyCafe.vue") },
+    ],
   },
   {
     path: "/users",
@@ -11,6 +14,7 @@ const routes = [
       { path: "login", component: () => import("pages/user/LoginPage.vue") },
       { path: "signup", component: () => import("pages/user/SignupPage.vue") },
       { path: "delete", component: () => import("pages/user/DeletePage.vue") },
+      { path: "cafedetail", component: () => import("pages/CafeDetail.vue") },
     ],
   },
   {
@@ -25,23 +29,10 @@ const routes = [
     component: () => import("layouts/SubLayout.vue"),
     children: [
       { path: "profile", component: () => import("pages/user/ProfilePage.vue") },
-      { path: "profile/edit", component: () => import("pages/user/EditUserPage.vue") }
+      { path: "profile/edit", component: () => import("pages/user/EditUserPage.vue") },
+      { path: "tag", component: () => import("pages/TagRecommend.vue") },
+      { path: "nearcafe", component: () => import("pages/NearCafe.vue") }
     ],
-  },
-  {
-    path: "/mycafe",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/MyCafe.vue") }],
-  },
-  {
-    path: "/tag",
-    component: () => import("layouts/SubLayout.vue"),
-    children: [{ path: "", component: () => import("pages/TagRecommend.vue") }],
-  },
-  {
-    path: "/cafedetail",
-    component: () => import("layouts/EmptyLayout.vue"),
-    children: [{ path: "", component: () => import("pages/CafeDetail.vue") }],
   },
 
   // Always leave this as last one,
