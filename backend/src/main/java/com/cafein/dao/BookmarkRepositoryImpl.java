d50f7111c7032abec67b3ff75fd8411ccac5f08a
co323co <co323co@gmail.com>
2021-10-05 01:02:05 +0900

    [S05P21B204-147] feat: Update bookmark dto

--- backend/src/main/java/com/cafein/dao/BookmarkRepositoryImpl.java
+++ backend/src/main/java/com/cafein/dao/BookmarkRepositoryImpl.java
@@ -5,8 +5,10 @@ import com.cafein.dto.bookmark.seleteBookmark.SelectBookmarkOutput;
 import com.cafein.entity.QBhour;
 import com.cafein.entity.QBookmark;
 import com.cafein.entity.QCafe;
+import com.cafein.entity.QReview;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,21 +23,25 @@ public class BookmarkRepositoryImpl implements BookmarkRepositoryCustom {
 
 	QBookmark qBookmark = QBookmark.bookmark;
     QCafe qCafe = QCafe.cafe;
-    QBhour qBhour = QBhour.bhour;
+    QReview qReview = QReview.review;
 
     @Override
     public Page<SelectBookmarkOutput> findByUserIdCustum(int userId, Pageable pageable) {
         QueryResults<SelectBookmarkOutput> queryResult = queryFactory
                 .select(new QSelectBookmarkOutput(qBookmark.id, Expressions.constant(userId), qCafe.id, qCafe.name,
                         qCafe.branch, qCafe.area, qCafe.tel, qCafe.address, qCafe.latitude, qCafe.longitude, qCafe.imgUrl,
-                        qBhour.type, qBhour.week_type, qBhour.mon, qBhour.tue, qBhour.wed, qBhour.thu, qBhour.fri,
-                        qBhour.sat, qBhour.sun, qBhour.startTime, qBhour.endTime, qBhour.etc
+                        // bookmarkCnt
+                        JPAExpressions.select(qBookmark.count().castToNum(Integer.class)).from(qBookmark)
+                                .where(qBookmark.cafe.id.eq(qCafe.id)),
+                        // reviewCnt
+                        JPAExpressions.select(qReview.count().castToNum(Integer.class)).from(qReview)
+                                .where(qReview.cafe.id.eq(qCafe.id))
                 ))
                 .from(qBookmark)
                 .join(qCafe)
                 .on(qBookmark.cafe.id.eq(qCafe.id))
-                .leftJoin(qBhour)
-                .on(qCafe.id.eq(qBhour.cafe.id))
+                .where(qBookmark.user.id.eq(userId))
+                .orderBy(qBookmark.id.desc()) //최근에 등록한 북마크 순
                 .offset(pageable.getOffset()).limit(pageable.getPageSize())
                 .fetchResults();
         long totalCount = queryResult.getTotal();
