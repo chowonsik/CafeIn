ec1d39ece3da95b6f05c34c3630f0c77d49e3a10
co323co <co323co@gmail.com>
2021-10-05 16:23:34 +0900

    [S05P21B204-212] feat: Add cafe curation by category api

--- backend/src/main/java/com/cafein/dao/CafeRepositoryImpl.java
+++ backend/src/main/java/com/cafein/dao/CafeRepositoryImpl.java
@@ -8,19 +8,18 @@ import com.cafein.dto.cafe.search.CafeSearchInput;
 import com.cafein.dto.cafe.search.QCafeSearchOutput;
 import com.cafein.dto.cafe.selectCafeDetail.QSelectCafeDetailOutput;
 import com.cafein.dto.cafe.selectCafeDetail.SelectCafeDetailOutput;
-import com.cafein.entity.QBhour;
-import com.cafein.entity.QBookmark;
-import com.cafein.entity.QCafe;
-import com.cafein.entity.QReview;
+import com.cafein.dto.cafe.suggest.QCafeCurationOutput;
+import com.cafein.dto.cafe.suggest.QSuggestByCategoryOutput;
+import com.cafein.dto.cafe.suggest.CafeCurationInput;
+import com.cafein.dto.cafe.suggest.CafeCurationOutput;
+import com.cafein.entity.*;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.Path;
 import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import org.hibernate.criterion.Projections;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -35,9 +34,9 @@ public class CafeRepositoryImpl implements CafeRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     QCafe qCafe = QCafe.cafe;
-	QBhour qBhour = QBhour.bhour;
 	QBookmark qBookmark = QBookmark.bookmark;
 	QReview qReview = QReview.review;
+	QTags qTags = QTags.tags;
 
 	@Override
 	public SelectCafeDetailOutput findByIdCustom(int id, int userId) {
@@ -94,6 +93,50 @@ public class CafeRepositoryImpl implements CafeRepositoryCustom {
 				.fetchResults();
 		long totalCount = queryResult.getTotal();
 		List<CafeSearchOutput> content = queryResult.getResults();
+
+		return new PageImpl<>(content, pageable, totalCount);
+	}
+
+	@Override
+	public Page<CafeCurationOutput> curationCafeByCategory(CafeCurationInput suggestByCategoryInput, int userId, Pageable pageable) {
+		double userLatitude = Double.parseDouble(suggestByCategoryInput.getLatitude());
+		double userLongitude = Double.parseDouble(suggestByCategoryInput.getLongitude());
+
+		QueryResults<CafeCurationOutput> queryResult = queryFactory
+				.select(new QCafeCurationOutput(qCafe.id, qCafe.name, qCafe.branch, qCafe.area, qCafe.tel, qCafe.address,
+						qCafe.latitude, qCafe.longitude,
+						//거리 구하기
+						Expressions.as(
+								acos(cos(radians(Expressions.constant(userLatitude)))
+										.multiply(cos(radians(qCafe.latitude.castToNum(Double.class))))
+										.multiply(cos(radians(qCafe.longitude.castToNum(Double.class)).subtract(radians(Expressions.constant(userLongitude)))))
+										.add((sin(radians(Expressions.constant(userLatitude))).multiply(sin(radians(qCafe.latitude.castToNum(Double.class))))))
+								).multiply(Expressions.constant(6371)).stringValue(),"distance"),
+						qCafe.imgUrl,
+						// isBookmark
+						JPAExpressions.select(qBookmark.count().castToNum(Integer.class)).from(qBookmark)
+								.where(qBookmark.user.id.eq(userId).and(qBookmark.cafe.id.eq(qCafe.id))),
+						// bookmarkCnt
+						JPAExpressions.select(qBookmark.count().castToNum(Integer.class)).from(qBookmark)
+								.where(qBookmark.cafe.id.eq(qCafe.id)),
+						// reviewCnt
+						JPAExpressions.select(qReview.count().castToNum(Integer.class)).from(qReview)
+								.where(qReview.cafe.id.eq(qCafe.id))
+				))
+				.from(qCafe)
+				.join(qTags)
+				.on(qCafe.id.eq(qTags.cafe.id))
+				//거리 제한보다 가까운 카페만
+				.where(acos(cos(radians(Expressions.constant(userLatitude)))
+						.multiply(cos(radians(qCafe.latitude.castToNum(Double.class))))
+						.multiply(cos(radians(qCafe.longitude.castToNum(Double.class)).subtract(radians(Expressions.constant(userLongitude)))))
+						.add((sin(radians(Expressions.constant(userLatitude))).multiply(sin(radians(qCafe.latitude.castToNum(Double.class))))))
+				).multiply(Expressions.constant(6371)).stringValue().loe(suggestByCategoryInput.getDistance()))
+				.orderBy(Expressions.stringPath(suggestByCategoryInput.getCategory()).desc(), Expressions.stringPath("distance").asc())
+				.offset(pageable.getOffset()).limit(pageable.getPageSize())
+				.fetchResults();
+		long totalCount = queryResult.getTotal();
+		List<CafeCurationOutput> content = queryResult.getResults();
 
 		return new PageImpl<>(content, pageable, totalCount);
 	}
