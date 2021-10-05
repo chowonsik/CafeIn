127b50d8976ba5e6a08cd21560f114f8312ffcaf
wscho94 <wscho94@gmail.com>
2021-10-05 16:35:43 +0900

    [S05P21B204-209] fix: Fix delete bookmark api and test code, api docs

--- backend/src/main/java/com/cafein/dao/BookmarkRepository.java
+++ backend/src/main/java/com/cafein/dao/BookmarkRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer>, BookmarkRepositoryCustom {
     Optional<Bookmark> findByUserAndCafe(User user, Cafe cafe);
+    Optional<Bookmark> findByUserIdAndCafeId(int userId, int cafeId);
 }
--- backend/src/main/java/com/cafein/service/BookmarkService.java
+++ backend/src/main/java/com/cafein/service/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.cafein.service;
 
 import com.cafein.dto.bookmark.createBookmark.CreateBookmarkInput;
+import com.cafein.dto.bookmark.deleteBookmark.DeleteBookmarkInput;
 import com.cafein.dto.bookmark.seleteBookmark.SelectBookmarkOutput;
 import com.cafein.response.PageResponse;
 import com.cafein.response.Response;
@@ -9,5 +10,5 @@ import org.springframework.http.ResponseEntity;
 public interface BookmarkService {
     ResponseEntity<Response<Object>> createBookmark(CreateBookmarkInput createBookmarkInput);
     ResponseEntity<PageResponse<SelectBookmarkOutput>> selectBookmarkListByUserId(int page, int size);
-    ResponseEntity<Response<Object>> deleteBookmark(int id);
+    ResponseEntity<Response<Object>> deleteBookmark(DeleteBookmarkInput deleteBookmarkInput);
 }
